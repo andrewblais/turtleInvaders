7de40fd87aa0d5fc50fc262298015137115364ba
andrewblais <anb2015@gmail.com>
2024-04-16 21:26:49 -0400

    Include functionality for firing defense and turtle destruction.

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 from turtle import Screen, Turtle
 import random
 from screeninfo import get_monitors
+import sys
 
 from config.config import *
+
+
+def is_collision_x_cond_met(obj, missile, space):
+    obj_x = obj.xcor()
+    obj_w = obj.shapesize()[1] * 1
+    x_cond = obj_x - (obj_w / 2 + space) <= missile.xcor() <= obj_x + (obj_w / 2 + space)
+    return x_cond
+
+
+def is_collision_y_cond_met(obj, missile, space):
+    obj_y = obj.ycor()
+    obj_h = obj.shapesize()[0] * 1
+    y_cond = obj_y - (obj_h / 2 + space) <= missile.ycor() <= obj_y + (obj_h / 2 + space)
+    return y_cond
+
+
+def are_collision_x_y_cond_met(obj, missile, space):
+    x_cond_met = is_collision_x_cond_met(obj, missile, space)
+    y_cond_met = is_collision_y_cond_met(obj, missile, space)
+    return x_cond_met and y_cond_met
 
 
 class TurtleInvaders:
@@ -20,10 +41,9 @@ class TurtleInvaders:
         self.formation_gutter_factor = .7  # Smaller val for bigger L-R gutters
         self.turtle_grid_width = 208
         self.turtle_grid_height = 208
-        self.cannon = None
-        self.cannon_start_x = -2
-        self.cannon_start_y = -350
-        self.turtle = None
+        self.player = None
+        self.player_start_x = -2
+        self.player_start_y = -350
         self.turtle_positions = []
         self.all_turtles = []
         self.score_object = None
@@ -40,6 +60,13 @@ class TurtleInvaders:
         self.uap_freq = random.randint(self.uap_sec_low, self.uap_sec_high) * .01
         self.turtle_movement_horiz = -5
         self.turtles_have_reversed = False  # Flag indicating turtles have changed direction
+        self.pause_game = False
+        self.game_on = True
+        self.player_missiles = []
+        self.player_has_fired = False
+        # self.player_reload_time = random.randint(2000, 5000)
+        self.player_reload_time = 2500
+        self.turtles_to_pop_ind = []
 
     def setup_screen(self):
         self.screen = Screen()
@@ -107,18 +134,18 @@ class TurtleInvaders:
                 return (value + round_number) % len(TURTLE_COLORS)
         raise ValueError(f"Number {number} not in any range key.")
 
-    def deploy_cannon(self):
-        self.cannon = Turtle()
-        self.cannon.shape('triangle')
-        self.cannon.turtlesize(stretch_wid=4, stretch_len=2, outline=5)
-        self.cannon.turtle_width = self.cannon.shapesize()[0]  # Get cannon width
-        self.cannon_start_x = -self.cannon.turtle_width / 2  # Center cannon
-        # Position cannon at comfortable y-coordinate:
-        self.cannon_start_y = -self.screen_height / 2 + self.screen_height / 15
-        self.cannon.color(PLAYER_COLOR)
-        self.cannon.penup()
-        self.cannon.setheading(90)
-        self.cannon.goto(self.cannon_start_x, self.cannon_start_y)
+    def deploy_player(self):
+        self.player = Turtle()
+        self.player.shape('triangle')
+        self.player.turtlesize(stretch_wid=4, stretch_len=2, outline=5)
+        self.player.turtle_width = self.player.shapesize()[0]  # Get player width
+        self.player_start_x = -self.player.turtle_width / 2  # Center player
+        # Position player at comfortable y-coordinate:
+        self.player_start_y = -self.screen_height / 2 + self.screen_height / 15
+        self.player.color(PLAYER_COLOR)
+        self.player.penup()
+        self.player.setheading(90)
+        self.player.goto(self.player_start_x, self.player_start_y)
 
     def inititalize_score(self):
         self.score_object = Turtle()
@@ -171,7 +198,7 @@ class TurtleInvaders:
         self.uap.goto(self.uap_x, self.uap_y)
 
     def uap_flyby(self):
-        print(self.time_tracker, self.uap_freq)
+        # print(self.time_tracker, self.uap_freq)
         if self.time_tracker > self.uap_freq:
             if self.uap_x > -self.screen_width / 2 - 50:
                 self.uap_x -= 10
@@ -182,8 +209,76 @@ class TurtleInvaders:
                 self.time_tracker = 0
                 self.uap_freq = random.randint(self.uap_sec_low, self.uap_sec_high) * .01
 
-    def cannon_fire(self):
+    def player_left(self):
+        if self.player.xcor() > -self.screen_width / 2 + 50:
+            x_pos = self.player.xcor() - 10
+            self.player.goto(x_pos, self.player.ycor())
+
+    def player_right(self):
+        if self.player.xcor() < self.screen_width / 2 - 55:
+            x_pos = self.player.xcor() + 10
+            self.player.goto(x_pos, self.player.ycor())
+
+    def reset_player_has_fired_flag(self):
+        self.player_has_fired = False
+
+    def player_fire(self):
+        if not self.player_has_fired:
+            player_missile = Turtle()
+            player_missile.shape('square')
+            player_missile.turtlesize(stretch_wid=.1, stretch_len=1.05, outline=2.5)
+            player_missile.penup()
+            player_missile.color(PLAYER_COLOR)
+            player_missile.setheading(90)
+            player_missile.goto(self.player.xcor(), self.player.ycor() + 35)
+            self.player_missiles.append(player_missile)
+            self.player_has_fired = True
+            self.screen.ontimer(self.reset_player_has_fired_flag,
+                                self.player_reload_time)
+
+    def player_missile_path(self):
+        for i in self.player_missiles:
+            player_x, player_y = i.xcor(), i.ycor()
+            i.goto(player_x, player_y + 30)
+            if player_y > self.screen_height / 2:
+                self.player_missiles.pop(self.player_missiles.index(i))
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            # # # # # # # # #
+            else:
+                for j in self.all_turtles:
+                    if are_collision_x_y_cond_met(j, i, 15):
+                        i.goto(-1500, 0)
+                        j.goto(-1500, 0)
+                        self.player_missiles.pop(self.player_missiles.index(i))
+                        self.all_turtles.pop(self.all_turtles.index(j))
+
+    def check_player_destroyed(self):
         pass
+
+    def restart_game(self):
+        self.screen.clear()
+        self.__init__()
+        self.begin_invasion()
+
+    def pause_toggle(self):
+        self.pause_game = not self.pause_game
+
+    def key_listeners(self):
+        """Creates key listeners for movement/pause_toggle/quit_game/restart_game methods."""
+        self.screen.listen()
+        self.screen.onkeypress(self.player_fire, 'space')
+        self.screen.onkeypress(self.player_left, 'Left')
+        self.screen.onkeypress(self.player_right, 'Right')
+        self.screen.onkeypress(self.pause_toggle, 'p')
+        self.screen.onkeypress(self.quit_game, 'q')
+        self.screen.onkeypress(self.restart_game, 'r')
 
     def turtle_change_dir(self):
         self.turtle_movement_horiz *= -1
@@ -223,15 +318,15 @@ class TurtleInvaders:
     def check_turtle_destroyed(self):
         pass
 
-    def check_cannon_destroyed(self):
-        pass
+    def quit_game(self):
+        self.screen.bye()
 
     def setup_pieces(self):
         self.inititalize_score()
         self.inititalize_lives()
         self.get_turtle_positions()
         self.turtle_formation()
-        self.deploy_cannon()
+        self.deploy_player()
         self.uap_sighting()
 
     def begin_invasion(self):
@@ -240,12 +335,23 @@ class TurtleInvaders:
         self.get_screen_width()
         self.setup_pieces()
         self.screen.tracer(2)
+        self.key_listeners()
         while self.lives_left > 0:
-            self.turtle_march()
-            self.uap_flyby()
+            while self.game_on:
+                if not self.pause_game:
+                    self.screen.update()
+                    self.turtle_march()
+                    self.player_missile_path()
+                    self.uap_flyby()
+                else:
+                    self.screen.update()
         self.screen.exitonclick()
 
 
 if __name__ == "__main__":
-    turtle_invaders = TurtleInvaders()
-    turtle_invaders.begin_invasion()
+    try:
+        turtle_invaders = TurtleInvaders()
+        turtle_invaders.begin_invasion()
+    except (KeyboardInterrupt, Exception) as e:
+        print(f"Error: {e}")
+        sys.exit(1)
